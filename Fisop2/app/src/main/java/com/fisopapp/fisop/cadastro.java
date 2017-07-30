dcacac1926658f04c1aefa3043f1e19a0d58ed39
Vedolin <vedolin@yahoo.com>
2017-07-30 08:44:59 -0300

    Mundancas

--- Fisop2/app/src/main/java/com/fisopapp/fisop/cadastro.java
+++ Fisop2/app/src/main/java/com/fisopapp/fisop/cadastro.java
@@ -1,13 +1,25 @@
 package com.fisopapp.fisop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class cadastro extends AppCompatActivity {
+
+public class Cadastro extends AppCompatActivity implements  View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        Button btnSolicitar = (Button) findViewById(R.id.btnSolicitar);
+        btnSolicitar.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+            Intent solicitarEmprestimo = new Intent(this, Analisis.class);
+            startActivity(solicitarEmprestimo);
     }
 }
