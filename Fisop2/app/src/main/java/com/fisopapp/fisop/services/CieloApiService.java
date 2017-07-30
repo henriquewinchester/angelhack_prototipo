acb00068840adf82dc1e11b303d74d1fe96e1eee
henrique <henrique.contato@outlook.com>
2017-07-30 09:17:33 -0300

    Alterando CieloApiService.java

--- Fisop2/app/src/main/java/com/fisopapp/fisop/services/CieloApiService.java
+++ Fisop2/app/src/main/java/com/fisopapp/fisop/services/CieloApiService.java
@@ -1,27 +1,35 @@
 package com.fisopapp.fisop.services;
 
-import android.os.AsyncTask;
-
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
+
 public class CieloApiService {
-    private final String BASE_URL = "https://api.cielo.com.br/order-management/v1/orders";
+    private HardCodedSecretsService secrets = new HardCodedSecretsService();
     private List<Object> orders = new ArrayList<Object>();
 
-    public List<Object> getAllOrders() {
+    public List<Object> getAllOrders() throws IOException {
+        String keys[] = secrets.getConfig();
 
-//        AsyncTask.execute(new Runnable() {
-//            @Override
-//            public void run() {
-//                URL endpoint = URL;
-//                HttpsURLConnection apiConnection = (HttpsURLConnection) endpoint.openConnection();
-//        });
+        URL endpoint = new URL(keys[3]);
+        HttpsURLConnection httpsConnection = (HttpsURLConnection)endpoint.openConnection();
+        httpsConnection.setRequestProperty("client-id", keys[0]);
+        httpsConnection.setRequestProperty("access-token", keys[1]);
+        httpsConnection.setRequestProperty("Merchant-Id", keys[2]);
+        httpsConnection.setRequestProperty("Content-Type", "application/json");
+
+        if (httpsConnection.getResponseCode() == 200) {
+            // Success
+            // Further processing here
+        } else {
+            // Error handling code goes here
+        }
 
         return new ArrayList<Object>();
     }
-
 }
--- Fisop2/app/src/main/java/com/fisopapp/fisop/services/HardCodedSecretsService.java
+++ Fisop2/app/src/main/java/com/fisopapp/fisop/services/HardCodedSecretsService.java
@@ -5,8 +5,10 @@ public class HardCodedSecretsService {
     private String clienteId = "mvKZV3XCNZj3";
     private String accessToken = "CGFxp2hKZf3m";
     private String merchantId = "a662f0a7-ead4-4e78-a042-94e442048d7c";
+    private String devBaseUrl = "https://api.cielo.com.br/sandbox-lio/order-management/v1";
+    private String prodBaseUrl = "https://api.cielo.com.br/order-management/v1";
 
     public String[] getConfig() {
-        return new String[] { clienteId, accessToken, merchantId };
+        return new String[] { clienteId, accessToken, merchantId, devBaseUrl, prodBaseUrl };
     }
 }
